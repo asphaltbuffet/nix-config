f247748ba5c026b34a9ab2fbcff4c43f1fd42e50
Ben Lechlitner <otherland@gmail.com>
2025-07-28 20:12:09 -0400

    configure/add starship prompt

--- home/common/programs.nix
+++ home/common/programs.nix
@@ -3,6 +3,7 @@
     ../programs/eza.nix
     ../programs/git.nix
     ../programs/fzf.nix
+    ../programs/starship.nix
     ../programs/tmux.nix
     ../programs/zoxide.nix
     ../programs/zsh.nix
