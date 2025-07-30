9fe05f062a038ff600e52a941cc0b10e3ef1a52b
Ben Lechlitner <otherland@gmail.com>
2025-07-30 17:56:18 -0400

    add wendigo host

---
+++ home/wendigo.nix
@@ -0,0 +1,70 @@
+{ lib, pkgs, ...}: {
+
+  imports = [
+    ./common
+  ];
+
+  targets.genericLinux.enable = true;
+  systemd.user.startServices = "sd-switch";
+
+  home = {
+    username = lib.mkDefault "grue";
+    homeDirectory = lib.mkDefault "/home/grue";
+    stateVersion = "25.05";
+    shell.enableZshIntegration = true;
+
+    packages = with pkgs; [
+      _1password-cli
+      asciinema
+      bat        # better cat
+      bottom     # better top
+      broot
+      choose     # cut/awk
+      delta      # better git-diff
+      doggo      # dig
+      duf        # better df
+      dust       # du + rust
+      fd         # better find
+      fping      # ping for mult
+      git-absorb
+      glow       # markdown viewer
+      gum        # fancy cli snippets
+      ijq        # interactive jq
+      iperf
+      jq
+      just
+      lnav
+      mcfly      # history search
+      moreutils
+      ncdu
+      pop        # email from cli
+      presenterm # presentations
+      ripgrep
+      taplo      # toml
+      tig        # git TUI
+      tmux
+      tmux-xpanes
+      trippy     # tui network tool
+      vhs        # terminal gifs
+      viddy      # better watch
+      wishlist   # ssh helper
+      xh         # better curl
+      zellij
+
+      _1password-gui
+      discord
+      kitty
+      signal-desktop
+      obsidian
+    ];
+  };
+
+  xdg.enable = true;
+
+  fonts.fontconfig.enable = true;
+
+  programs.home-manager.enable = true;
+  programs.firefox.enable = true;
+
+
+}
