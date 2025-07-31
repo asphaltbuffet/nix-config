9cd7f3eb2b99847bb4530569d48bed6798a29027
Ben Lechlitner <otherland@gmail.com>
2025-07-31 12:35:21 -0400

    Change name to match user

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -21,7 +21,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.grue = {
     isNormalUser = true;
-    description = "ben";
+    description = "grue";
     shell = pkgs.zsh;
     extraGroups = [ "networkmanager" "wheel" ];
   };
