086c7dac4fb4567b8f6836089a9373fc9d6bdfaa
Ben Lechlitner <otherland@gmail.com>
2025-09-09 11:06:09 -0400

    disable wifi powersave

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -1,18 +1,19 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS        = "en_US.UTF-8";
+    LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT    = "en_US.UTF-8";
-    LC_MONETARY       = "en_US.UTF-8";
-    LC_NAME           = "en_US.UTF-8";
-    LC_NUMERIC        = "en_US.UTF-8";
-    LC_PAPER          = "en_US.UTF-8";
-    LC_TELEPHONE      = "en_US.UTF-8";
-    LC_TIME           = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 
   # Enable CUPS to print documents.
@@ -20,9 +21,9 @@
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users = {
-    grue    = import ../users/grue/default.nix;
+    grue = import ../users/grue/default.nix;
     jsquats = import ../users/jsquats/default.nix;
-    sukey   = import ../users/sukey/default.nix;
+    sukey = import ../users/sukey/default.nix;
   };
 
   users.defaultUserShell = pkgs.zsh;
@@ -51,13 +52,18 @@
     wget
 
     nixfmt-rfc-style
-    nh
   ];
+
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+  };
 
   programs.vim = {
     enable = true;
     defaultEditor = true;
-    package = (pkgs.vim_configurable.override {  }).customize{
+    package = (pkgs.vim_configurable.override { }).customize {
       name = "vim";
       # Install plugins for example for syntax highlighting of nix files
       vimrcConfig.packages.myplugins = with pkgs.vimPlugins; {
@@ -69,8 +75,8 @@
           vim-commentary
           vim-repeat
           vim-unimpaired
-      ];
-        opt = [];
+        ];
+        opt = [ ];
       };
       vimrcConfig.customRC = ''
         set gcr=a:blinkon0
