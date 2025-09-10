e45f85e18f7a3f494f03c4457603c32940d46b13
Ben Lechlitner <otherland@gmail.com>
2025-09-10 15:31:48 -0400

    add prismlauncher on kushtaka

--- home/users/jsquats/kushtaka.nix
+++ home/users/jsquats/kushtaka.nix
@@ -1,4 +1,37 @@
-{ config, pkgs, ... }:
+# .../home/users/jsquats/kushtaka.nix
+{ lib, pkgs, ... }:
 {
+
+  imports = [
+    ../../common
+  ];
+
+  targets.genericLinux.enable = true;
+  systemd.user.startServices = "sd-switch";
+
+  home = {
+    username = "jsquats";
+    homeDirectory = "/home/jsquats";
+    stateVersion = "25.05";
+    shell.enableZshIntegration = true;
+
+    packages = with pkgs; [
+      _1password-cli
+      _1password-gui
+      discord
+      flameshot
+      kitty
+      prismlauncher
+      signal-desktop
+      vlc
+    ];
+  };
+
+  xdg.enable = true;
+
+  fonts.fontconfig.enable = true;
+
+  programs.home-manager.enable = true;
+  programs.firefox.enable = true;
 
 }
