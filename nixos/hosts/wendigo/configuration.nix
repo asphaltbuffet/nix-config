268bbeb50ac50a3ee4ce6ec5ea18c4de32b1fa9c
Ben Lechlitner <otherland@gmail.com>
2025-10-02 15:20:20 -0400

    remove shared data from host configs

--- nixos/hosts/wendigo/configuration.nix
+++ nixos/hosts/wendigo/configuration.nix
@@ -1,76 +1,20 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports = [
     ./hardware-configuration.nix
 
-    ../common
-    ../common/firefox.nix
+    ../../common/users.nix
+
+    ../../profiles/base.nix
+    ../../profiles/dev.nix
+    ../../profiles/gaming.nix
+    ../../profiles/laptop/t14.nix
   ];
 
   networking.hostName = "wendigo"; # Define your hostname.
 
-  # Set your time zone.
-  time.timeZone = "America/New_York";
-
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
-  # enable fingerprint scanner
-  #services.fprintd.enable = true;
-  #security.pam.services.login.fprintAuth = true;
-  #security.pam.services.xscreensaver.fprintAuth = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    options = "caps:swapescape";
-    variant = "";
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    #jack.enable = true;
-
-  };
-
-  # Install firefox.
-  # programs.firefox.enable = true;
-
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-  };
-
 }
