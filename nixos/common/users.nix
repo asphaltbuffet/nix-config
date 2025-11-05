d840ae474108ddb0e6a9ca6062b9cd12fb6343b5
Ben Lechlitner <30903912+asphaltbuffet@users.noreply.github.com>
2025-11-04 20:31:13 -0500

    temporary remove sukey user

--- nixos/common/users.nix
+++ nixos/common/users.nix
@@ -17,15 +17,15 @@
     shell = pkgs.bash;
   };
 
-  users.users.sukey = {
-    isNormalUser = true;
-    description = "sukey";
-    extraGroups = [ "networkmanager" ];
-    shell = pkgs.bash;
-  };
+  # users.users.sukey = {
+  #   isNormalUser = true;
+  #   description = "sukey";
+  #   extraGroups = [ "networkmanager" ];
+  #   shell = pkgs.bash;
+  # };
 
   # Attach Home-Manager configs
   home-manager.users.grue = import ../../home/users/grue.nix;
   home-manager.users.jsquats = import ../../home/users/jsquats.nix;
-  home-manager.users.sukey   = import ../../home/users/sukey.nix;
+  # home-manager.users.sukey = import ../../home/users/sukey.nix;
 }
