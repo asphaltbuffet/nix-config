888bea3a4a10c28b7d293eae114836f972e7eaa3
Ben Lechlitner <30903912+asphaltbuffet@users.noreply.github.com>
2026-03-10 17:55:30 -0400

    fix: add user pubkey from kushtaka

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,9 @@
 let
-  grue = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBCNN0FY6PqVhfejv10JDfq56G1DTR4RWNjPpt/LSNRN";
-  users = [grue];
+  grue = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBCNN0FY6PqVhfejv10JDfq56G1DTR4RWNjPpt/LSNRN" # wendigo
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEzLYqZEgfo5DqhCtJMN3sDvS5DO9Vxa6rvkQh7T1ZZv" # kushtaka
+  ];
+  users = grue;
 
   wendigo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDyrkGOX0lDcdIO5ehmjTzRhW9UEJwXRnFYAYbsFHz76 root@wendigo";
   kushtaka = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKfVXEd5gyLbgYnmmi9yrGL8zQcU2v8iXioIlSsCzZ57";
