5d5482d4e832d7b83d5e9a1c08577cf7def273a6
Ben Lechlitner <30903912+asphaltbuffet@users.noreply.github.com>
2026-03-22 09:55:47 -0400

    enable auto-deploy via nixos-autodeploy

--- nixos/hosts/snallygaster/configuration.nix
+++ nixos/hosts/snallygaster/configuration.nix
@@ -10,4 +10,8 @@
 
   networking.hostName = "snallygaster";
   system.stateVersion = "26.05";
+
+  # Pull NixOS updates automatically from CI via cachix + GitHub Pages.
+  # See .autodeploy-skip/snallygaster to pause without editing this file.
+  system.autoDeploy.enable = true;
 }
