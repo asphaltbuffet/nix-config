8e8fb5b61e74fa53ba8af8f1af1bca9ae940446d
Ben Lechlitner <30903912+asphaltbuffet@users.noreply.github.com>
2026-03-22 13:07:29 -0400

    feat: enable autodeploy on kushtaka

--- nixos/hosts/kushtaka/configuration.nix
+++ nixos/hosts/kushtaka/configuration.nix
@@ -14,4 +14,8 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
+
+  # Pull NixOS updates automatically from CI via cachix + GitHub Pages.
+  # See .autodeploy-skip/snallygaster to pause without editing this file.
+  system.autoDeploy.enable = true;
 }
