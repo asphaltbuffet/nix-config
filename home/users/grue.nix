2595047db8c711a759874863a65546a2fd6ba0fa
Ben Lechlitner <30903912+asphaltbuffet@users.noreply.github.com>
2026-03-24 12:20:39 -0400

    combine jj/git user info

--- home/users/grue.nix
+++ home/users/grue.nix
@@ -1,5 +1,10 @@
 # home/users/grue.nix
-{pkgs, ...}: {
+{pkgs, ...}: let
+  identity = {
+    name = "Ben Lechlitner";
+    email = "30903912+asphaltbuffet@users.noreply.github.com";
+  };
+in {
   imports = [
     ../roles/base.nix
     ../roles/admin.nix
@@ -13,17 +18,9 @@
   home.homeDirectory = "/home/grue";
   home.stateVersion = "25.05";
 
-  programs.git.settings.user = {
-    name = "Ben Lechlitner";
-    email = "30903912+asphaltbuffet@users.noreply.github.com";
-  };
+  programs.git.settings.user = identity;
 
-  programs.jujutsu.settings = {
-    user = {
-      name = "Ben Lechlitner";
-      email = "30903912+asphaltbuffet@users.noreply.github.com";
-    };
-  };
+  programs.jujutsu.settings.user = identity;
 
   # Personal touches
   home.packages = with pkgs; [
