f80baf7c6c7a7a00c0275b639990a1f77f20a3ca
Ben Lechlitner <30903912+asphaltbuffet@users.noreply.github.com>
2026-03-26 17:30:37 -0400

    feat: enable wendigo autodeploy

--- nixos/hosts/snallygaster/configuration.nix
+++ nixos/hosts/snallygaster/configuration.nix
@@ -1,17 +1,20 @@
 {...}: {
   imports = [
     ./hardware-configuration.nix
+    ../../profiles/laptop/x1carbon.nix
 
     ../../common/users.nix
 
     ../../profiles/base.nix
-    ../../profiles/laptop/x1carbon.nix
   ];
 
   networking.hostName = "snallygaster";
+
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "26.05";
 
   # Pull NixOS updates automatically from CI via cachix + GitHub Pages.
-  # See .autodeploy-skip/snallygaster to pause without editing this file.
+  # Create .autodeploy-skip/snallygaster to pause without editing this file.
   system.autoDeploy.enable = true;
 }
