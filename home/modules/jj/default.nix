9acd4a6b67f18032924ff081efdaeda020a93923
Ben Lechlitner <30903912+asphaltbuffet@users.noreply.github.com>
2026-04-04 11:41:42 -0400

    feat: custom jj log output

--- home/modules/jj/default.nix
+++ home/modules/jj/default.nix
@@ -10,6 +10,35 @@
         diff-editor = ":builtin";
       };
 
+      templates.log = ''
+        label(
+          separate(" ",
+            if(current_working_copy, "working_copy"),
+            if(immutable, "immutable", "mutable"),
+            if(conflict, "conflicted"),
+          ),
+          concat(
+            separate(" ",
+              change_id.shortest(8),
+              commit_id.shortest(8),
+              committer.timestamp().ago(),
+              bookmarks.join(" "),
+              tags,
+              working_copies,
+              if(author.email() != "30903912+asphaltbuffet@users.noreply.github.com",
+                label("author", "‹" ++ author.name() ++ "›")),
+            ) ++ "\n",
+            "  " ++ separate(" ",
+              if(empty, label("empty", "(empty)")),
+              if(description,
+                description.first_line(),
+                label(if(empty, "empty"), "(no description set)"),
+              ),
+            ) ++ "\n",
+          )
+        )
+      '';
+
       aliases = {
         tug = ["bookmark" "move" "--from" "heads(::@- & bookmarks())" "--to" "@-"];
       };
