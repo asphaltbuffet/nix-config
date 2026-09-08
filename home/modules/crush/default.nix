63b9741a8cdde0c5e271654f5c322c9688641fa8
Ben Lechlitner <30903912+asphaltbuffet@users.noreply.github.com>
2026-09-08 18:14:22 -0400

    fix: remove duplicate crush decl

--- home/modules/crush/default.nix
+++ home/modules/crush/default.nix
@@ -1,9 +1,5 @@
 # home/modules/crush/default.nix
-{inputs, ...}: {
-  imports = [
-    inputs.charmbracelet-nur.homeModules.crush
-  ];
-
+{...}: {
   programs.crush = {
     enable = true;
     settings = {
